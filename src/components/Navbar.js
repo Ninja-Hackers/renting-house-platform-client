539f30995ffb6159ee85b0e9be8ab1147bfe1924
HaridhaRavi <haridha.gr@gmail.com>
2022-11-01 23:01:49 +0100

    improve NavBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,45 +6,69 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 
+const navBtn = {
+  backgroundColor: "#7fdff5",
+  borderRadius: "6px",
+  color: "white",
+  margin: "0rem 0.3rem",
+  fontWeight: "medium",
+  padding:"0.5rem 1rem"
+};
+
 function NavBar() {
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
   return (
-    <Navbar bg='light' expand='lg'>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand as={Link} to='/'>
-          Home
+        <Navbar.Brand as={Link} to="/">
+          <img
+            src="https://www.seekpng.com/png/full/187-1875894_cottage-drawing-pretty-house-modern-house-vector-house.png"
+            width="120"
+            height="30"
+            alt="Home logo"
+          />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='me-auto'>
-            <Nav.Link as={Link} to='/houses'>
+
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/houses">
               Houses
             </Nav.Link>
             {isLoggedIn && (
               <>
-                <NavDropdown title='My Details' id='basic-nav-dropdown'>
-                  <NavDropdown.Item as={Link} to='/my-houses'>
+                <Nav.Link as={Link} to="/create-house">
+                  Become a Host
+                </Nav.Link>
+                <NavDropdown title="My Details" id="collasible-nav-dropdown">
+                  <NavDropdown.Item as={Link} to="/my-houses">
                     My Houses
                   </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to='/my-reservations'>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item as={Link} to="/my-reservations">
                     My reservations
                   </NavDropdown.Item>
                 </NavDropdown>
-                <Nav.Link as={Link} to='/'>
-                  {user && user.name}
+              </>
+            )}
+          </Nav>
+          <Nav>
+            {isLoggedIn && (
+              <>
+                <Nav.Link as={Link} to="/">
+                  <strong> Hello, {user && user.name}!!!</strong>
                 </Nav.Link>
-                <Nav.Link as={Link} onClick={logOutUser} to='/'>
+                <Nav.Link as={Link} onClick={logOutUser} style={navBtn} to="/">
                   Logout
                 </Nav.Link>
               </>
             )}
-
             {!isLoggedIn && (
               <>
-                <Nav.Link as={Link} to='/login'>
+                <Nav.Link as={Link} style={navBtn} to="/login">
                   Login
                 </Nav.Link>
-                <Nav.Link as={Link} to='/signup'>
+                <Nav.Link as={Link} style={navBtn} to="/signup">
                   Signup
                 </Nav.Link>
               </>
