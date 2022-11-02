a123153f69a3d3a1df18a5137ba861fb2a382aef
HaridhaRavi <haridha.gr@gmail.com>
2022-11-02 20:56:00 +0100

    improve homePage styling with bootstrap

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,18 @@
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import { Form, Button } from "react-bootstrap";
+
+const searchBtn = {
+  backgroundColor: "#7fdff5 !important",
+  borderRadius: "6px",
+  color: "white",
+  margin: "0.3rem 0.3rem",
+  fontWeight: "medium",
+  padding: "0.5rem 1rem",
+};
 
 function HomePage() {
   return (
     <div>
-      <Form className='d-flex'>
+      {/* <Form className='d-flex'>
         <Form.Control
           type='search'
           placeholder='Search'
@@ -15,7 +23,105 @@ function HomePage() {
       </Form>
       <div className='home-page'>
         <h1>Home page</h1>
+      </div> */}
+      <div className="card text-bg-dark mb-3">
+        <img
+          src="https://images.unsplash.com/photo-1568605114967-8130f3a36994?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          className="card-img"
+          alt="..."
+        />
+        <div className="card-img-overlay">
+          <h5 className="card-title fs-1">Find it, rent it, live there</h5>
+          <p className="card-text mb-5 fs-5 fst-italic">
+            Let us guide you home
+          </p>
+          <section>
+            <Form className="d-flex mt-5 pt-5">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button className="text-white" variant="primary">
+                Search
+              </Button>
+            </Form>
+          </section>
+        </div>
       </div>
+
+      <footer className="bg-light">
+        <div className="row mb-3">
+          <p className="card-text text-black fst-italic">
+            Your perfect place to live, work, or study is just one click away!
+            <br />
+            Find the houses that gives you all the freedom you need - to get one
+            step closer to achieving your goals.
+          </p>
+        </div>
+        <div className="row row-cols-1 row-cols-md-3 g-4 ">
+          <div className="col text-white mb-2">
+            <div className="card h-100 bg-dark d-felx align-items-center">
+              <img
+                src="./verifyList-home.svg"
+                className="card-img-top"
+                alt="..."
+                style={{ width: "20%" }}
+              />
+              <div className="card-body">
+                <h5 className="card-title">Verified Apartments</h5>
+                <p className="card-text">
+                  We verify listings and visit apartments to make sure you get
+                  exactly the home you saw on the pictures.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col text-white mb-2">
+            <div className="card h-100 bg-dark d-felx align-items-center">
+              <img
+                src="./home-footer-document-icon.svg"
+                className="card-img-top"
+                alt="..."
+                style={{ width: "20%", alignItems: "center" }}
+              />
+              <div className="card-body">
+                <h5 className="card-title">Smart Contracts</h5>
+                <p className="card-text">
+                  Chose your apartment and rent it by signing the contract
+                  online - all of our processes are optimised to save you time.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col text-white mb-2">
+            <div className="card h-100 bg-dark d-felx align-items-center">
+              <img
+                src="./customer-home-icon.svg"
+                className="card-img-top"
+                alt="..."
+                style={{ width: "20%", alignItems: "center" }}
+              />
+              <div className="card-body">
+                <h5 className="card-title">Personal Support</h5>
+                <p className="card-text">
+                  We are always here to help you and offer you individual
+                  support on every step of the way to your HB Homes. <br />
+                  📞 +49 1523334562
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12 text-center bg-dark pt-2">
+            <p className="text-white">
+              Built with love © 2022 by Haridha and Berkay
+            </p>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
