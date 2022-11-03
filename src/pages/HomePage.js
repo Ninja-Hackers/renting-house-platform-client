79e91058df4c5b8fb58a39535355945ef5c7ca90
HaridhaRavi <haridha.gr@gmail.com>
2022-11-03 21:38:20 +0100

    add placeholder to searchbar

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -20,31 +20,31 @@ function HomePage({ callbackToSearch }) {
 
   return (
     <div>
-      <div className='card text-bg-dark mb-3'>
+      <div className="card text-bg-dark mb-3">
         <img
-          src='https://images.unsplash.com/photo-1568605114967-8130f3a36994?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-          className='card-img'
-          alt='...'
+          src="https://images.unsplash.com/photo-1568605114967-8130f3a36994?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          className="card-img"
+          alt="..."
         />
-        <div className='card-img-overlay'>
-          <h5 className='card-title fs-1'>Find it, rent it, live there</h5>
-          <p className='card-text mb-5 fs-5 fst-italic'>
+        <div className="card-img-overlay">
+          <h5 className="card-title fs-1">Find it, rent it, live there</h5>
+          <p className="card-text mb-5 fs-5 fst-italic">
             Let us guide you home
           </p>
-          <section className='d-flex justify-content-center'>
+          <section className="d-flex justify-content-center">
             <form onSubmit={handleSubmit}>
-              <FormGroup role='form' className='d-flex col-6 pt-3 w-100 p-3'>
+              <FormGroup role="form" className="d-flex col-6 pt-3 w-100 p-3">
                 <Form.Control
-                  type='search'
-                  placeholder='Search'
-                  className='me-2'
-                  aria-label='Search'
+                  type="search"
+                  placeholder="Enter the city"
+                  className="me-2"
+                  aria-label="Search"
                   value={searchInput}
                   onChange={(e) => {
                     inputHandler(e);
                   }}
                 />
-                <Button className='text-white' variant='primary' type='submit'>
+                <Button className="text-white" variant="primary" type="submit">
                   Search
                 </Button>
               </FormGroup>
@@ -53,70 +53,70 @@ function HomePage({ callbackToSearch }) {
         </div>
       </div>
 
-      <footer className='bg-light'>
-        <div className='row mb-3'>
-          <p className='card-text text-black fst-italic'>
+      <footer className="bg-light">
+        <div className="row mb-3">
+          <p className="card-text text-black fst-italic">
             Your perfect place to live, work, or study is just one click away!
             <br />
             Find the houses that gives you all the freedom you need - to get one
             step closer to achieving your goals.
           </p>
         </div>
-        <div className='row row-cols-1 row-cols-md-3 g-4 '>
-          <div className='col text-white mb-2 '>
+        <div className="row row-cols-1 row-cols-md-3 g-4 ">
+          <div className="col text-white mb-2 ">
             <div
-              className='card h-100 bg-dark d-felx align-items-center'
+              className="card h-100 bg-dark d-felx align-items-center"
               style={{ borderRadius: "2rem" }}
             >
               <img
-                src='./verifyList-home.svg'
-                className='card-img-top'
-                alt='...'
+                src="./verifyList-home.svg"
+                className="card-img-top"
+                alt="..."
                 style={{ width: "20%" }}
               />
-              <div className='card-body'>
-                <h5 className='card-title'>Verified Apartments</h5>
-                <p className='card-text'>
+              <div className="card-body">
+                <h5 className="card-title">Verified Apartments</h5>
+                <p className="card-text">
                   We verify listings and visit apartments to make sure you get
                   exactly the home you saw on the pictures.
                 </p>
               </div>
             </div>
           </div>
-          <div className='col text-white mb-2'>
+          <div className="col text-white mb-2">
             <div
-              className='card h-100 bg-dark d-felx align-items-center'
+              className="card h-100 bg-dark d-felx align-items-center"
               style={{ borderRadius: "2rem" }}
             >
               <img
-                src='./home-footer-document-icon.svg'
-                className='card-img-top'
-                alt='...'
+                src="./home-footer-document-icon.svg"
+                className="card-img-top"
+                alt="..."
                 style={{ width: "20%", alignItems: "center" }}
               />
-              <div className='card-body'>
-                <h5 className='card-title'>Smart Contracts</h5>
-                <p className='card-text'>
+              <div className="card-body">
+                <h5 className="card-title">Smart Contracts</h5>
+                <p className="card-text">
                   Chose your apartment and rent it by signing the contract
                   online - all of our processes are optimised to save you time.
                 </p>
               </div>
             </div>
           </div>
-          <div className='col text-white mb-2'>
+          <div className="col text-white mb-2">
             <div
-              className='card h-100 bg-dark d-felx align-items-center'
+              className="card h-100 bg-dark d-felx align-items-center"
               style={{ borderRadius: "2rem" }}
             >
               <img
-                src='./customer-home-icon.svg'
-                className='card-img-top'
-                alt='...'
+                src="./customer-home-icon.svg"
+                className="card-img-top"
+                alt="..."
                 style={{ width: "20%", alignItems: "center" }}
               />
-              <div className='card-body'>
-                <h5 className='card-title'>Personal Support</h5>
-                <p className='card-text'>
+              <div className="card-body">
+                <h5 className="card-title">Personal Support</h5>
+                <p className="card-text">
                   We are always here to help you and offer you individual
                   support on every step of the way to your HB Homes. <br />
                   📞 +49 1523334562
@@ -125,9 +125,9 @@ function HomePage({ callbackToSearch }) {
             </div>
           </div>
         </div>
-        <div className='row'>
-          <div className='col-md-12 text-center bg-dark pt-2'>
-            <p className='text-white'>
+        <div className="row">
+          <div className="col-md-12 text-center bg-dark pt-2">
+            <p className="text-white">
               Built with love © 2022 by Haridha and Berkay
             </p>
           </div>
