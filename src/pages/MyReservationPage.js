0c5e8d165f872dd5b3db6342b161fce3f1a09282
berkayyildirim <huseyinberkayyildirim@gmail.com>
2022-11-04 12:05:19 +0300

    fix MyReservationPage data error

--- src/pages/MyReservationPage.js
+++ src/pages/MyReservationPage.js
@@ -21,25 +21,25 @@ function MyReservationPage() {
   }, []);
 
   return (
+    <div
+      className='row row-cols-0 row-cols-md-2 row-cols-lg-2 g-0 mh-100'
+      style={{ backgroundColor: "#11222b" }}
+    >
+      {reservations.length >= 1 ? (
+        reservations.map((reservation) => (
+          <ReservationDetailsCard key={reservation._id} {...reservation} />
+        ))
+      ) : (
         <div
-          className="row row-cols-0 row-cols-md-2 row-cols-lg-2 g-0 mh-100"
-          style={{ backgroundColor: "#11222b" }}
+          className='d-flex justify-content-center mt-3'
+          style={{ width: "100%" }}
         >
-          {reservations.length > 1 ? (
-            reservations.map((reservation) => (
-              <ReservationDetailsCard key={reservation._id} {...reservation} />
-            ))
-          ) : (
-            <div
-              className="d-flex justify-content-center mt-3"
-              style={{ width: "100%" }}
-            >
-              <Alert key="primary" variant="primary">
-                <p className="fs-5 m-2">There are no Reservations</p>
-              </Alert>
-            </div>
-          )}
+          <Alert key='primary' variant='primary'>
+            <p className='fs-5 m-2'>There are no Reservations</p>
+          </Alert>
         </div>
+      )}
+    </div>
   );
 }
 
