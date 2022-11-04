1bf80f25c462cada5c3154e72a65b94b0b01b256
berkayyildirim <huseyinberkayyildirim@gmail.com>
2022-11-04 11:50:49 +0300

    add a message after filtering to HouseListPage

--- src/pages/HouseListPage.js
+++ src/pages/HouseListPage.js
@@ -3,14 +3,19 @@ import "./HouseListPage.css";
 
 function HouseListPage({ filteredhouses }) {
   return (
-    <div style={{ backgroundColor: "#11222b"}}>
-      <div className='product-container'>
-        <div className='container' >
-          <div className='row flex-row'>
-            {filteredhouses &&
+    <div style={{ backgroundColor: "#11222b" }}>
+      <div className='product-container' style={{ minHeight: "50rem" }}>
+        <div className='container' style={{ minHeight: "25rem" }}>
+          <div className='row flex-row' style={{ minHeight: "20rem" }}>
+            {filteredhouses && filteredhouses.length >= 1 ? (
               filteredhouses.map((house) => (
                 <HouseCard key={house._id} {...house} />
-              ))}
+              ))
+            ) : (
+              <h1 style={{ color: "white", padding: "10rem" }}>
+                There are no houses matching with your search
+              </h1>
+            )}
           </div>
         </div>
       </div>
